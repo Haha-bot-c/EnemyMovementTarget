825410d68646814fc1eca696ec2bd406379ab3bc
Haha-bot-c <Akypant4023@gmail.com>
2024-05-10 13:06:42 +0700

    init

--- Assets/Script/EnemyPursuer.cs
+++ Assets/Script/EnemyPursuer.cs
@@ -7,15 +7,12 @@ public class EnemyPursuer : MonoBehaviour
     private const float _stoppingDistance = 2f;
     private const float _rotationSpeed = 5f;
     private const float _smoothTime = 0.1f;
-
     [SerializeField] private PlayerMover _target;
     [SerializeField] private float _speed = 3f;
     [SerializeField] private float _jumpForce = 3f;
     [SerializeField] private float _pushForce = 3f;
-
     private Animator _animator;
     private Rigidbody _rigidBody;
-
     private Vector3 _currentVelocity;
     private Vector3 _move;
 
@@ -24,6 +21,7 @@ public class EnemyPursuer : MonoBehaviour
         _rigidBody = GetComponent<Rigidbody>();
         _animator = GetComponent<Animator>();
     }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.TryGetComponent(out Obstacle obstacle))
@@ -34,11 +32,11 @@ public class EnemyPursuer : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Movement();
+        MoveTowardsTarget();
         LookAtTarget();
     }
 
-    private void Movement()
+    private void MoveTowardsTarget()
     {
         Vector3 direction = (_target.transform.position - transform.position).normalized;
         _move = Vector3.SmoothDamp(_rigidBody.velocity, direction * _speed, ref _currentVelocity, _smoothTime);
--- Assets/Script/PlayerMover.cs
+++ Assets/Script/PlayerMover.cs
@@ -3,27 +3,22 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterController), typeof(Animator))]
 public class PlayerMover : MonoBehaviour
 {
-    private const string Horizontal = "Horizontal";
-    private const string Vertical = "Vertical";
+    private const string HorizontalInput = "Horizontal";
+    private const string VerticalInput = "Vertical";
     private const string CommandJump = "Jump";
     private const string MouseXAxisName = "Mouse X";
     private const string MouseYAxisName = "Mouse Y";
-    
     private const float GroundCheckRadius = 0.1f;
     private const float GroundCheckDistance = -2f;
     private const float GravityMultiplier = -2f;
-    private const int MiddleMouseButton = 2;
-
+    private const int RotateMouseButton = 2;
     [SerializeField] private LayerMask _groundMask;
     [SerializeField] private float _speed = 5f;
     [SerializeField] private float _jumpHeight = 2f;
-    [SerializeField] private float _gravity = -9.81f;
-    
+    [SerializeField] private float _gravity = -9.81f; 
     private float _rotationSpeed = 2f;
     private bool _isGrounded;
-
     private Vector3 _velocity;
-
     private CharacterController _controller;
     private Animator _animator;
 
@@ -32,49 +27,38 @@ public class PlayerMover : MonoBehaviour
         _controller = GetComponent<CharacterController>();
         _animator = GetComponent<Animator>();
     }
-    
+
     private void Update()
     {
         Vector3 currentPosition = transform.position;
-
-        HandleMovement(ref currentPosition);
-  
+        HandleMovement(currentPosition);
         HandleRotation();
         Jump();
     }
 
-    private void HandleMovement(ref Vector3 position)
+    private void HandleMovement(Vector3 position)
     {
         _animator.SetBool("Grounded", _isGrounded);
-        
         Vector3 forwardDirection = transform.forward;
         Vector3 rightDirection = transform.right;
-
         forwardDirection.y = 0f;
         rightDirection.y = 0f;
-
         forwardDirection.Normalize();
         rightDirection.Normalize();
-
-        float horizontalInput = Input.GetAxis(Horizontal);
-        float verticalInput = Input.GetAxis(Vertical);
+        float horizontalInput = Input.GetAxis(HorizontalInput);
+        float verticalInput = Input.GetAxis(VerticalInput);
         Vector3 move = rightDirection * horizontalInput + forwardDirection * verticalInput;
-
         _controller.Move(move * _speed * Time.deltaTime);
-        position += move * _speed * Time.deltaTime;
-
         _animator.SetFloat("MoveSpeed", verticalInput);
-       
     }
 
-    
+
     private void HandleRotation()
     {
-        if (Input.GetMouseButton(MiddleMouseButton))
+        if (Input.GetMouseButton(RotateMouseButton))
         {
             float mouseX = Input.GetAxis(MouseXAxisName);
             float mouseY = Input.GetAxis(MouseYAxisName);
-
             transform.Rotate(Vector3.up, mouseX * _rotationSpeed, Space.World);
             transform.Rotate(Vector3.left, mouseY * _rotationSpeed, Space.Self);
         }
@@ -94,7 +78,6 @@ public class PlayerMover : MonoBehaviour
             _isGrounded = false;
             _velocity.y = Mathf.Sqrt(_jumpHeight * GravityMultiplier * _gravity);
         }
-
         _velocity.y += _gravity * Time.deltaTime;
         _controller.Move(_velocity * Time.deltaTime);
     }
